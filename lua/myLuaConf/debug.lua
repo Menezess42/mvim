381c6556a4c9e1e2ac92b2085668d5fad78767d7
Menezess42 <arielmenezess@hotmail.com>
2026-01-13 16:50:50 -0300

    Change somme keybinds 4 the dap

--- lua/myLuaConf/debug.lua
+++ lua/myLuaConf/debug.lua
@@ -24,20 +24,20 @@ require('lze').load {
       local dap = require 'dap'
       local dapui = require 'dapui'
 
-      vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-      vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-      vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-      vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+      vim.keymap.set('n', '<leader>dd', dap.continue, { desc = 'Debug: Start/Continue' })
+      vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'Debug: Step Into' })
+      vim.keymap.set('n', '<leader>dn', dap.step_over, { desc = 'Debug: Step Over' })
+      vim.keymap.set('n', '<leader>do', dap.step_out, { desc = 'Debug: Step Out' })
       vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
       vim.keymap.set('n', '<leader>B', function()
         dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
       end, { desc = 'Debug: Set Breakpoint' })
 
-      vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+      vim.keymap.set('n', '<leader>ld', dapui.toggle, { desc = 'Debug: See last session result.' })
 
       dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+      -- dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      -- dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
       dapui.setup {
         icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
@@ -95,7 +95,7 @@ require('lze').load {
                 for_cat = { cat = 'debug.python', default = false },
                 on_plugin = { "nvim-dap", },
                 after = function(plugin)
-                        require("dap-python").setup()
+                        require("dap-python").setup("python")
                 end,
         },
 }
