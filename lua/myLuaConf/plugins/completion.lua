d9c0fa2d48f5da5a4088f33b4498d1e3ff01b47d
Menezess42 <arielmenezess@hotmail.com>
2026-01-19 14:54:41 -0300

    ...

--- lua/myLuaConf/plugins/completion.lua
+++ lua/myLuaConf/plugins/completion.lua
@@ -45,9 +45,8 @@ return {
         after = function(_)
             require("blink.cmp").setup({
                 keymap = {
-                        ['<Tab>'] = { 'move_next', 'snippet_forward', 'fallback' },
+                        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
                         ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
-                        ['<CR>'] = {},
                 },
                 cmdline = {
                     enabled = true,
